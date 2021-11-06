40d2c2218fc8d525b3730e9da81b3391063b23f7
DJ Taylor <dtaylor88@gmail.com>
2021-11-06 10:51:02 -0700

    add about page and link component

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <p>Hi there! Gatsby is pretty rad.</p>
+        </div>
+    );
+};
+
+export default about;
