cc80eb48689a4027cdf9a34b5337278a250d5aa6
DJ Taylor <dtaylor88@gmail.com>
2021-11-07 12:48:22 -0800

    add dynamic page route using mdx slug

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts">
+            <p>Blog post contents will go here eventually...</p>
+        </Layout>
+    );
+};
+
+export default BlogPost;
