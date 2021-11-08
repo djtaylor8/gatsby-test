eb8b43638663bfae1d329bddc0cd9715d5ae0305
DJ Taylor <dtaylor88@gmail.com>
2021-11-07 20:14:48 -0800

    render dynamic images using gatsbyimage

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Layout from '../../components/layout';
 import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 
 const BlogPost = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image);
     return (
         <Layout pageTitle="Super Cool Blog Posts">
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage 
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -15,13 +22,21 @@ const BlogPost = ({ data }) => {
 };
 
 export const query = graphql`
-query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
